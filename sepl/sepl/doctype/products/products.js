e5df61911f2c549ee05bc13e75d72c6c91ca74b6
RahulSarkar <bluephoenix00995@gmail.com>
2024-09-27 18:07:08 +0530

    feat: calculations fixed

--- sepl/sepl/doctype/products/products.js
+++ sepl/sepl/doctype/products/products.js
@@ -1,27 +1,54 @@
-// Copyright (c) 2024, Thunder and contributors
-// For license information, please see license.txt
+frappe.ui.form.on('Products', {
+    refresh: function(frm) {
+        frm.fields_dict['item_details'].grid.get_field('item_code').get_query = function(doc, cdt, cdn) {
+            return {
+                filters: {
+                    'is_stock_item': 1
+                }
+            };
+        };
+    }
+});
 
-// frappe.ui.form.on("Products", {
-// 	refresh(frm) {
+frappe.ui.form.on('Item Details', {
+    item_code: function(frm, cdt, cdn) {
+        let row = locals[cdt][cdn];
+        if (row.item_code) {
+            frappe.call({
+                method: 'get_item_details',
+                doc: frm.doc,
+                args: {
+                    item_code: row.item_code
+                },
+                callback: function(r) {
+                    if (r.message) {
+                        // Store the current qty
+                        let currentQty = row.qty;
+                        
+                        frappe.model.set_value(cdt, cdn, {
+                            'item_name': r.message.item_name,
+                            'uom': r.message.uom,
+                            'category': r.message.category,
+                            'thicknesssq_mm': r.message.thicknesssq_mm,
+                            'length_ft': r.message.length_ft,
+                            'color': r.message.color,
+                            'brand': r.message.brand,
+                            'rate': r.message.rate,
+                            'size': r.message.size,
+                            'qty': currentQty || r.message.qty // Use current qty if it exists, otherwise use from item master
+                        });
+                    }
+                }
+            });
+        }
+    },
+    
+    qty: function(frm, cdt, cdn) {
+        // This function will be triggered when qty is changed manually
+        let row = locals[cdt][cdn];
+        if (row.rate && row.qty) {
+            frappe.model.set_value(cdt, cdn, 'amount', row.rate * row.qty);
+        }
+    }
+});
 
-// 	},
-// });
-
-
-
-// frappe.ui.form.on('YourParentDoctype', {
-//     after_save: function(frm) {
-//         let total_amount = 0;
-
-//         // Iterate over each row in the child table and sum up the amount
-//         $.each(frm.doc.item_details || [], function(i, row) {
-//             total_amount += row.amount;
-//         });
-
-//         // Set the total amount in the material_cost field
-//         frm.set_value('material_cost', total_amount);
-
-//         // Refresh the field to show the updated value
-//         frm.refresh_field('material_cost');
-//     }
-// });
--- sepl/sepl/doctype/products/products.py
+++ sepl/sepl/doctype/products/products.py
@@ -24,44 +24,72 @@ class Products(Document):
         self.labour_cost = total_amount
 
     def calculate_material_costs(self):
+        material_cost = self.material_cost or 0
+        self.value_after_material_discount = self.safe_multiply(material_cost, (self.material_discount or 0) / 100)
+        self.value_after_material_operating_margins = self.safe_multiply(material_cost, (self.material_operating_margins or 0) / 100)
+        self.value_after_material_tool_charges = self.safe_multiply(material_cost, (self.material_tool_charges or 0) / 100)
+        self.value_after_insurance = self.safe_multiply(material_cost, (self.insurance or 0) / 100)
+        self.value_after_transportation = self.safe_multiply(material_cost, (self.transportation or 0) / 100)
+        self.value_after_material_wastage = self.safe_multiply(material_cost, (self.material_wastage or 0) / 100)
+        self.value_after_miscellaneous = self.safe_multiply(material_cost, (self.miscellaneous or 0) / 100)
+
         self.overhead_cost = sum([
-            self.material_operating_margins or 0,
-            self.material_tool_charges or 0,
-            self.insurance or 0,
-            self.transportation or 0,
-            self.material_wastage or 0,
-            self.miscellaneous or 0
+            self.value_after_material_discount,
+            self.value_after_material_operating_margins,
+            self.value_after_material_tool_charges,
+            self.value_after_insurance,
+            self.value_after_transportation,
+            self.value_after_material_wastage,
+            self.value_after_miscellaneous
         ])
-        self.total_material_and_over_head_cost = (self.overhead_cost or 0) + (self.material_cost or 0)
+        self.total_material_and_over_head_cost = material_cost + self.overhead_cost
 
     def calculate_labour_costs(self):
+        labour_cost = self.labour_cost or 0
+        self.value_after_labour_discount = self.safe_multiply(labour_cost, (self.labour_discount or 0) / 100)
+        self.value_after_labour_operating_margins = self.safe_multiply(labour_cost, (self.labour_operating_margins or 0) / 100)
+        self.value_after_labour_tool_charges = self.safe_multiply(labour_cost, (self.labour_tool_charges or 0) / 100)
+        self.value_after_engineering_charges = self.safe_multiply(labour_cost, (self.engineering_charges or 0) / 100)
+        self.value_after_supervision_charges = self.safe_multiply(labour_cost, (self.supervision_charges or 0) / 100)
+        self.value_after_admin_charges = self.safe_multiply(labour_cost, (self.admin_charges or 0) / 100)
+        self.value_after_labour_miscellaneous = self.safe_multiply(labour_cost, (self.labour_miscellaneous or 0) / 100)
+
         self.total_labour_overhead = sum([
-            self.labour_operating_margins or 0,
-            self.labour_tool_charges or 0,
-            self.engineering_charges or 0,
-            self.supervision_charges or 0,
-            self.admin_charges or 0,
-            self.labour_miscellaneous or 0
+            self.value_after_labour_discount,
+            self.value_after_labour_operating_margins,
+            self.value_after_labour_tool_charges,
+            self.value_after_engineering_charges,
+            self.value_after_supervision_charges,
+            self.value_after_admin_charges,
+            self.value_after_labour_miscellaneous
         ])
-        self.total_overhead_and_labour_cost = (self.total_labour_overhead or 0) + (self.labour_cost or 0)
+        self.total_overhead_and_labour_cost = labour_cost + self.total_labour_overhead
+
 
     def calculate_final_costs(self):
-        # Calculate final material cost after discount
-        material_discount_factor = 1 - (self.material_discount or 0) / 100
-        self.final_material_cost = self.total_material_and_over_head_cost * material_discount_factor
-
-        # Calculate final labour cost after discount
-        labour_discount_factor = 1 - (self.labour_discount or 0) / 100
-        self.final_labour_cost = self.total_overhead_and_labour_cost * labour_discount_factor
-
-        # Calculate final cost
-        self.final_cost = self.final_material_cost + self.final_labour_cost
+        self.final_cost = self.total_overhead_and_labour_cost + self.total_material_and_over_head_cost
 
     @staticmethod
     def safe_multiply(a, b):
         if a is None or b is None:
             return 0
         return float(a) * float(b)
+
+    @frappe.whitelist()
+    def get_item_details(self, item_code):
+        item = frappe.get_doc("Item", item_code)
+        return {
+            "item_name": item.item_name,
+            "uom": item.stock_uom,
+            "category": item.custom_category,
+            "thicknesssq_mm": item.custom_thicknesssqmm,
+            "length_ft": item.custom_lengthft,
+            "color": item.custom_color,
+            "brand": item.brand,
+            "rate": item.custom_rate,
+            "size": item.custom_size,
+            "qty": item.custom_qty
+        }
 
 def calculate_child_table_totals(doc, method):
     for item in doc.item_details:
